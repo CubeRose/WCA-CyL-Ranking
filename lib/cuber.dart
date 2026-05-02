314ed952eb94380c0d4e29cd17b1b12b48558e24
hvocep00 <hvocep00@estudiantes.unileon.es>
2026-05-02 12:14:42 +0200

    Cambiado Cuber para que guarde todos los resultados con una sola request

--- lib/cuber.dart
+++ lib/cuber.dart
@@ -1,19 +1,56 @@
 class Cuber {
   String id;
   String name;
-  double time;
+  Map<String, int> singles;
+  Map<String, int> averages;
 
   Cuber({
     required this.id,
     required this.name,
-    required this.time,
-  });
+    Map<String, int>? singles,
+    Map<String, int>? averages,
+  }) : singles = singles ?? {},
+       averages = averages ?? {};
 
-  factory Cuber.fromJson(Map<String, dynamic> json, String name) {
+  factory Cuber.fromJson(Map<String, dynamic> personJson, String name) {
+    final id = personJson['id'] ?? personJson['personId'] ?? '';
+
+    final Map<String, int> singlesMap = {};
+    final Map<String, int> averagesMap = {};
+
+    if (personJson.containsKey('rank') && personJson['rank'] is Map) {
+      final rank = personJson['rank'] as Map<String, dynamic>;
+
+      if (rank['singles'] is List) {
+        for (final item in rank['singles'] as List) {
+          if (item is Map<String, dynamic>) {
+            final ev = item['eventId'] as String? ?? item['event'] as String?;
+            final best = item['best'];
+            if (ev != null && best is num) {
+              singlesMap[ev] = best.toInt();
+            }
+          }
+        }
+      }
+
+      if (rank['averages'] is List) {
+        for (final item in rank['averages'] as List) {
+          if (item is Map<String, dynamic>) {
+            final ev = item['eventId'] as String? ?? item['event'] as String?;
+            final best = item['best'];
+            if (ev != null && best is num) {
+              averagesMap[ev] = best.toInt();
+            }
+          }
+        }
+      }
+    }
+
     return Cuber(
-      id: json['personId'],
-      time: json['best'] / 100,
+      id: id as String,
       name: name,
+      singles: singlesMap,
+      averages: averagesMap,
     );
   }
 }
